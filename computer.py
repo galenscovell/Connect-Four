b9163dbdbfe5366dd6dc5089982ba1cc07c9b352
Galen Scovell <galen.scovell@gmail.com>
2015-01-07 20:38:52 -0800

    Begin project, GUI setup

---
+++ computer.py
@@ -0,0 +1,8 @@
+
+
+class Computer:
+    'Controls AI mechanics'
+
+    def __init__(self):
+        self.name = "Hadron, the Collider"
+        self.wins = 0
---
+++ player.py
@@ -0,0 +1,8 @@
+
+
+class Player:
+    'Controls human player mechanics'
+
+    def __init__(self, name):
+        self.name = name
+        self.wins = 0
