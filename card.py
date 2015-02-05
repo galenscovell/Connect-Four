46370af39b783c609a5d73df76aebad9dab27a69
Galen Scovell <galen.scovell@gmail.com>
2015-02-04 20:23:53 -0800

    Update documentation/docstrings

--- card.py
+++ card.py
@@ -2,7 +2,7 @@
 import random
 
 class Card:
-    'Individual card info'
+    """Individual card object."""
     instances = []
     guesses = []
     correct = []
--- memory.py
+++ memory.py
@@ -15,8 +15,6 @@ CELL_MARGIN  = 10
 X_MARGIN = int((WINDOW_X - (BOARD_X * (CELLSIZE + CELL_MARGIN))) / 2)
 Y_MARGIN = int((WINDOW_Y - (BOARD_Y * (CELLSIZE + CELL_MARGIN))) / 2)
 
-assert (BOARD_X * BOARD_Y) % 2 == 0, "Board must have even number of boxes in order to complete game."
-
 DONUT   = 'donut'
 SQUARE  = 'square'
 DIAMOND = 'diamond'
@@ -29,23 +27,21 @@ BOARD       = ( 82, 179, 217)
 BOARDSHADOW = ( 41, 128, 185)
 HIDDEN      = ( 75, 119, 190)
 CARDSHADOW  = ( 44,  62,  80)  
-HIGHLIGHT   = (236, 240, 241)  # Cloud White
+HIGHLIGHT   = (236, 240, 241) 
 
-COLOR_1    = ( 39, 174,  96)  # Nephritis Green
+COLOR_1    = ( 39, 174,  96)
 TINT_1     = ( 46, 204, 113)
-COLOR_2    = (241, 196,  15)  # Sunflower Yellow
+COLOR_2    = (241, 196,  15)
 TINT_2     = (243, 156,  18)
-COLOR_3    = ( 52, 152, 219)  # Peter River Blue
+COLOR_3    = ( 52, 152, 219)
 TINT_3     = ( 41, 128, 185)
-COLOR_4    = (231,  76,  60)  # Alizarin Red
+COLOR_4    = (231,  76,  60)
 TINT_4     = (192,  57,  43)
 
 
 #---------- List Constants ----------#
 SHAPE_LIST = (DONUT, SQUARE, DIAMOND, CIRCLE)
 COLOR_LIST = (COLOR_1, COLOR_2, COLOR_3, COLOR_4)
-
-assert len(SHAPE_LIST) * len(COLOR_LIST) * 2 == (BOARD_X * BOARD_Y), "Board options must match board size."
 
 
 #---------- Pygame init ----------#
@@ -56,8 +52,10 @@ pygame.display.set_caption('Memory')
 
 
 class Game:
-    #---------- Board Creation/Updating ----------#
+    """Main game class."""
+    
     def create_icon_list(self):
+        """Create randomized list of icons and colors for card placement."""
         icon_list = []
         for color in COLOR_LIST:
             for shape in SHAPE_LIST:
@@ -67,7 +65,9 @@ class Game:
         random.shuffle(icon_list)
         return icon_list
 
+
     def create_board(self, icons):
+        """Create board grid, draw board graphics, create card objects."""
         board = []
         for x in range(BOARD_X):
             column = []
@@ -76,11 +76,23 @@ class Game:
                 column.append(card)
                 del icons[0]
             board.append(column)
-        pygame.draw.rect(DISPLAY, BOARDSHADOW, [X_MARGIN / 2, Y_MARGIN / 2, WINDOW_X - X_MARGIN, WINDOW_Y - Y_MARGIN + 4])
-        pygame.draw.rect(DISPLAY, BOARD, [X_MARGIN / 2, Y_MARGIN / 2, WINDOW_X - X_MARGIN, WINDOW_Y - Y_MARGIN])
+        pygame.draw.rect(DISPLAY, BOARDSHADOW, [
+            X_MARGIN / 2, 
+            Y_MARGIN / 2, 
+            WINDOW_X - X_MARGIN, 
+            WINDOW_Y - Y_MARGIN + 4
+            ])
+        pygame.draw.rect(DISPLAY, BOARD, [
+            X_MARGIN / 2, 
+            Y_MARGIN / 2, 
+            WINDOW_X - X_MARGIN, 
+            WINDOW_Y - Y_MARGIN
+            ])
         return board
 
+
     def update_board(self, board):
+        """Redraw card graphics with frame updates/user selection."""
         for card in Card.instances:
             if card.mode == 0:
                 self.draw_card(card, HIDDEN)
@@ -91,23 +103,29 @@ class Game:
                 self.draw_card_icon(card)
 
 
-    #---------- Card Creation/Updating ----------#
     def draw_card(self, card, color):
+        """Draw base card graphics."""
         pygame.draw.rect(DISPLAY, CARDSHADOW, [
             (CELL_MARGIN + CELLSIZE) * card.x + X_MARGIN + (CELL_MARGIN / 2), 
             (CELL_MARGIN + CELLSIZE) * card.y + Y_MARGIN + (CELL_MARGIN / 2) + CELLSIZE - 1, 
-            CELLSIZE, 4])
+            CELLSIZE, 4
+            ])
         card.rect = pygame.draw.rect(DISPLAY, color, [
             (CELL_MARGIN + CELLSIZE) * card.x + X_MARGIN + (CELL_MARGIN / 2), 
             (CELL_MARGIN + CELLSIZE) * card.y + Y_MARGIN + (CELL_MARGIN / 2), 
-            CELLSIZE, CELLSIZE])
+            CELLSIZE, CELLSIZE
+            ])
+
 
     def left_top_coords(self, card):
+        """Determine upper left rectangle coordinates of card for draw functions."""
         left = int((CELL_MARGIN + CELLSIZE) * card.x + X_MARGIN + (CELL_MARGIN / 2))
         top = int((CELL_MARGIN + CELLSIZE) * card.y + Y_MARGIN + (CELL_MARGIN / 2))
         return (left, top)
 
+
     def draw_card_icon(self, card):
+        """Draw card color and icon."""
         if card.color == COLOR_1:
             tint = TINT_1
         elif card.color == COLOR_2:
@@ -120,32 +138,58 @@ class Game:
         quarter = int(CELLSIZE * 0.25)
         left, top = self.left_top_coords(card)
         if card.icon == DONUT:
-            pygame.draw.circle(DISPLAY, tint, (left + half, top + half), half - 10)
-            pygame.draw.circle(DISPLAY, card.color, (left + half, top + half), quarter - 2)
+            pygame.draw.circle(DISPLAY, tint, 
+                (left + half, top + half), half - 10)
+            pygame.draw.circle(DISPLAY, card.color, 
+                (left + half, top + half), quarter - 2)
         elif card.icon == SQUARE:
-            pygame.draw.rect(DISPLAY, tint, (left + quarter, top + quarter, CELLSIZE - half, CELLSIZE - half))
+            pygame.draw.rect(DISPLAY, tint, 
+                (left + quarter, top + quarter, CELLSIZE - half, CELLSIZE - half))
         elif card.icon == DIAMOND:
-            pygame.draw.polygon(DISPLAY, tint, ((left + half, top + 6), (left + CELLSIZE - 10, top + half + 4), (left + half, top + CELLSIZE - 8), (left + 10, top + half + 4)))
+            pygame.draw.polygon(DISPLAY, tint, (
+                (left + half, top + 6), 
+                (left + CELLSIZE - 10, top + half + 4), 
+                (left + half, top + CELLSIZE - 8), 
+                (left + 10, top + half + 4)
+                ))
         elif card.icon == CIRCLE:
             pygame.draw.circle(DISPLAY, tint, (left + half, top + half), half - 10)
 
+
     def hide_card(self, card, clock):
-        for coverage in range(0, 60, 4):
-            pygame.draw.rect(DISPLAY, HIDDEN, [(CELL_MARGIN + CELLSIZE) * card.x + X_MARGIN + (CELL_MARGIN / 2), (CELL_MARGIN + CELLSIZE) * card.y + Y_MARGIN + (CELL_MARGIN / 2), CELLSIZE, coverage + 4])
+        """Card hide animation for game beginning and incorrect guesses."""
+        for coverage in range(0, 60, 6):
+            pygame.draw.rect(DISPLAY, HIDDEN, [
+                (CELL_MARGIN + CELLSIZE) * card.x + X_MARGIN + (CELL_MARGIN / 2), 
+                (CELL_MARGIN + CELLSIZE) * card.y + Y_MARGIN + (CELL_MARGIN / 2), 
+                CELLSIZE, coverage + 6
+                ])
             pygame.display.flip()
             clock.tick(FPS)
         card.mode = 0
 
+
     def show_card(self, card, clock):
+        """Card flip animation on user selection."""
         for coverage in range(0, 60, 6):
-            pygame.draw.rect(DISPLAY, card.color, [(CELL_MARGIN + CELLSIZE) * card.x + X_MARGIN + (CELL_MARGIN / 2), (CELL_MARGIN + CELLSIZE) * card.y + Y_MARGIN + (CELL_MARGIN / 2), CELLSIZE, CELLSIZE])
-            pygame.draw.rect(DISPLAY, HIDDEN, [(CELL_MARGIN + CELLSIZE) * card.x + X_MARGIN + (CELL_MARGIN / 2), (CELL_MARGIN + CELLSIZE) * card.y + Y_MARGIN + (CELL_MARGIN / 2), CELLSIZE, 60 - coverage])
+            pygame.draw.rect(DISPLAY, card.color, [
+                (CELL_MARGIN + CELLSIZE) * card.x + X_MARGIN + (CELL_MARGIN / 2), 
+                (CELL_MARGIN + CELLSIZE) * card.y + Y_MARGIN + (CELL_MARGIN / 2), 
+                CELLSIZE, CELLSIZE
+                ])
+            pygame.draw.rect(DISPLAY, HIDDEN, [
+                (CELL_MARGIN + CELLSIZE) * card.x + X_MARGIN + (CELL_MARGIN / 2), 
+                (CELL_MARGIN + CELLSIZE) * card.y + Y_MARGIN + (CELL_MARGIN / 2), 
+                CELLSIZE, 60 - coverage
+                ])
             pygame.display.flip()
             clock.tick(FPS)
         Card.guesses.append(card)
         card.mode = 2
 
+
     def update_text(self):
+        """Redraw font object as guesses correctly guessed."""
         remaining = str(round((len(Card.instances) - len(Card.correct)) / 2))
         fontObj = pygame.font.Font('OpenSans-Light.ttf', 24)
         textObj = fontObj.render('Remaining: ' + remaining, True, HIGHLIGHT, BACKGROUND)
@@ -154,8 +198,9 @@ class Game:
         DISPLAY.fill(BACKGROUND, (0, 0, WINDOW_X, 50))
         DISPLAY.blit(textObj, textRect)
 
-    #---------- Guessing ----------#
+    
     def check_guess(self, clock):
+        """Check for win event when two guesses selected."""
         pygame.time.wait(500)
         first, second = Card.guesses[0], Card.guesses[1]
         if first.color == second.color and first.icon == second.icon:
@@ -173,6 +218,8 @@ class Game:
 
 def main():
     clock = pygame.time.Clock()
+
+    # Board creation
     creating_board = True
     while creating_board:
         DISPLAY.fill(BACKGROUND)
@@ -183,10 +230,12 @@ def main():
         game.update_text()
         creating_board = False
 
+    # Card begin animation
     random.shuffle(Card.instances)
     for card in Card.instances:
         game.hide_card(card, clock)
 
+    # Main game loop
     guesses = 0
     running = True
     startTime = time.time()
